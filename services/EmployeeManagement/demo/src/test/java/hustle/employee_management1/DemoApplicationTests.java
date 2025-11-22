c70619a50b0711cfa5540063758ddf606492cef2
ThanhfVux2102 <24560017@gm.uit.edu.vn>
2025-11-23 01:14:39 +0700

    feat: structured with maven

---
+++ services/EmployeeManagement/demo/src/test/java/hustle/employee_management1/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hustle.employee_management1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
