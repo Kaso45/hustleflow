37257850fdf1579b3374212161fc10aad83177d5
ThanhfVux2102 <24560017@gm.uit.edu.vn>
2025-11-27 18:32:02 +0700

    feat: status reporting for task management

---
+++ backend/app/src/main/java/com/hustleflow/task/dto/TaskStatusUpdateRequest.java
@@ -0,0 +1,15 @@
+package com.hustleflow.task.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TaskStatusUpdateRequest {
+
+    @NotBlank
+    private String status; // "DONE", "IN_PROGRESS", ...
+
+    private String completionNote;
+}
